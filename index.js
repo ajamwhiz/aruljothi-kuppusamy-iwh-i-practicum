5d1660b2c021b33be04b767643fdd7796b3ba12f
Aruljothi K <jak.jothi@gmail.com>
2025-03-23 16:05:16 +0530

    simple custom object List and Create

--- index.js
+++ index.js
@@ -2,70 +2,63 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+require('dotenv').config()
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_ACCESS_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+// ROUTE 1: GET for the homepage to display the list of Pet custom objects
+app.get('/', async (req, res) => {
+    const petsUrl = 'https://api.hubapi.com/crm/v3/objects/2-140672354?properties=name,breed,species';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        const response = await axios.get(petsUrl, { headers });
+
+        const customObjects = response.data.results;
+        res.render('homepage', { title: 'Pet Records | HubSpot', customObjects });
+    } catch (error) {
+        console.error(error);
+        res.send('Error fetching Pet data');
     }
-
 });
-*/
 
+// ROUTE 2: GET for the form to create or update a Pet custom object
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | HubSpot Integration' });
+});
+
+// ROUTE 3: POST to handle form submission and create a new Pet record
+app.post('/update-cobj', async (req, res) => {
+    const newPet = {
+        properties: {
+            name: req.body.name,
+            species: req.body.species,
+            breed: req.body.breed,
+        }
+    };
+
+    const petsUrl = 'https://api.hubspot.com/crm/v3/objects/2-140672354';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(petsUrl, newPet, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.send('Error creating new Pet record');
+    }
+});
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
